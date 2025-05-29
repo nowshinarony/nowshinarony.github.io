ae69550e6afc25aae88609f7cabf449c7a8c9921
Nowshin Nawar Arony <arony.nowshin@gmail.com>
2025-05-29 11:23:54 -0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-4xl font-bold text-purple-600">Hello Portfolio!</h1>
+    </div>
+  )
+}
+
+export default App
